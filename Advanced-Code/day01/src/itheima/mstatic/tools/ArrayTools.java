4b5432e288f9bd5ffbcd429ca30bb4129695ae5d
ymh <ymh@guigu.com>
2024-03-20 14:16:01 +0800

    second commit

--- Advanced-Code/day01/src/itheima/mstatic/tools/ArrayTools.java
+++ Advanced-Code/day01/src/itheima/mstatic/tools/ArrayTools.java
@@ -4,5 +4,6 @@ public class ArrayTools {
     public static void main(String[] args) {
 Coder c = new Coder();
         System.out.println(c.getAge());
+        System.out.println("hello world");
     }
 }
