b1ca540e72949c6325b0eed14ffe9afaa95c0934
ymh <ymh@guigu.com>
2024-03-31 10:33:53 +0800

    案例1

--- itheima_web_project/tlias/src/main/java/com/itheima/service/impl/TliasApplication.java
+++ itheima_web_project/tlias/src/main/java/com/itheima/service/impl/TliasApplication.java
@@ -2,6 +2,8 @@ package com.itheima.service.impl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
 
 @SpringBootApplication
 public class TliasApplication {
